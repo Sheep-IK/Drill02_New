7badbe0f401d7395808e81d17f06075aff521557
Sheep-IK-LT <yik418@naver.com>
2023-09-21 12:33:31 +0900

    사각형 중 오른쪽 줄 완료

--- character_grass.py
+++ character_grass.py
@@ -32,8 +32,13 @@ def run_rectangle():
     print('RECTANGLE')
     
     #bottom line
-    for x in range(50 , 750+1, 5):
-        render_frame(x, 90) #x, y 위치에 캐릭터 그려줄 수 있는 함수
+   for x in range(50 , 750+1, 5):
+      render_frame(x, 90) #x, y 위치에 캐릭터 그려줄 수 있는 함수
+
+    #right line
+    for y in range(90, 550+1, 5):
+        render_frame(750, y) 
+
         
     #top line
     for x in range(750, 50-1, -5):
