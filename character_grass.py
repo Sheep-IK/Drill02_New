d409ff690110e3366620802a6ff12e90daf22d7f
Sheep-IK-LT <yik418@naver.com>
2023-09-21 12:30:49 +0900

    사각형 중 윗줄 완료

--- character_grass.py
+++ character_grass.py
@@ -20,15 +20,12 @@ def render_frame(x, y):
 
 def run_circle():
     print('CIRCLE')
-
     cx, cy, r = 400, 300, 200
    
     for deg in range(0 , 360, 5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
         render_frame(x, y)
-
-    pass
 
 
 def run_rectangle():
@@ -38,10 +35,9 @@ def run_rectangle():
     for x in range(50 , 750+1, 5):
         render_frame(x, 90) #x, y 위치에 캐릭터 그려줄 수 있는 함수
         
-
-    
-    pass
-
+    #top line
+    for x in range(750, 50-1, -5):
+        render_frame(x, 550)
 
 
 
